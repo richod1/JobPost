c01007412e56f3f9cc63d34ca5a26a57e1e71876
Degraft Frimpong <frimpsup@gmail.com>
2024-06-25 13:16:42 -0400

    image-upload added

---
+++ src/app/components/ImageUpload.tsx
@@ -0,0 +1,61 @@
+'use client'
+import axios from "axios"
+import Image from "next/image"
+import {Button} from "@radix-ui/themes"
+import {ChangeEvent,useRef,useState} from 'react'
+import {faSpinner,faUser,IconDefinition} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+export default function ImageUpload({name,icon,defaultValue=''}:{
+    name:string,
+    icon:IconDefinition,
+    defaultValue:string
+}){
+    const fileInRef=useRef<HTMLInputElement>(null);
+    const [isUploading,setIsUploading]=useState(false);
+    const [isImageLoading,setIsImageLoading]=useState(false);
+    const [url,setUrl]=useState(defaultValue)
+
+
+    async function upload(event:ChangeEvent<HTMLInputElement>){
+        const input=event.target as HTMLInputElement;
+        if(input && input.files?.length && input.files.length > 0){
+            setIsUploading(true);
+            const file=input.files[0];
+            const data=new FormData;
+            data.set('file',file);
+            const response=await axios.post('/api/upload',data);
+
+            if(response.data.url){
+                setUrl(response.data.url);
+                setIsUploading(false);
+                setIsImageLoading(true)
+            }
+        }
+    }
+
+    const imgLoading=(isUploading || isImageLoading);
+    return(
+        <>
+        <div className="bg-gray-100 rounded-md size-24 inline-flex items-center content-center justify-center">
+            {imgLoading && (
+                <FontAwesomeIcon icon={faSpinner} className="text-gray-400 animate-spin"/>
+            )}
+            {(!isUploading) && url &&(
+                <Image src={url} alt={'uploaded image'} width={1024} height={1024} onLoadingComplete={()=>setIsImageLoading(false)} className="w-auto h-auto max-h-24"/>
+            )}
+            {!imgLoading && !url &&(
+                <FontAwesomeIcon icon={icon} className="text-gray-400"/>
+            )}
+        </div>
+        <input type="hidden" value={url} name={name}/>
+        <div className="mt-2">
+            <input onChange={event=>upload(event)} ref={fileInRef} 
+            type="file" 
+            className="hidden"/>
+            <Button type="button" onClick={()=>fileInRef.current?.click()} variant="soft">select file</Button>
+        </div>
+        </>
+
+    )
+}
